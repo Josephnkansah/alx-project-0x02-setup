3e94b7664afcd48cd61dc80f997b9697530c64ff
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2025-06-05 20:31:06 -1200

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../components/layout/Header';
 import UserCard from '../components/common/UserCard';
 import { UserProps } from '../interfaces';
 
-const UsersPage = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-      });
-  }, []);
-
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <>
       <Header />
       <main className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-6">Users</h1>
-        {users.map((user, index) => (
-          <UserCard
-            key={index}
-            name={user.name}
-            email={user.email}
-            address={user.address}
-          />
-        ))}
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {users.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
       </main>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: UserProps[] = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 };
 
 export default UsersPage;
